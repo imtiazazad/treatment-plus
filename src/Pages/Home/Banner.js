1dd6f6176d67feb612e57f622194311ed19bff55
imtiazazad <imtiaz16-549@diu.edu.com>
2022-06-28 00:44:33 +0600

    service added

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -3,16 +3,30 @@ import React from 'react';
 
 const Banner = () => {
     return (
-        <div class="hero min-h-screen bg-base-200">
-  <div class="hero-content flex-col lg:flex-row-reverse">
-    <img src="https://i.ibb.co/qpVW8v3/holly-mandarich-UVy-Of-X3v0-Ls-unsplash.jpg?w=260&h=400" class="max-w-sm rounded-lg shadow-2xl" />
-    <div>
-      <h1 class="text-5xl font-bold">Helping You Stay Happy One</h1>
-      <p class="py-6">Checkout Our Service</p>
-      <button class="btn btn-primary">Check Out</button>
+      <div class="carousel w-full">
+      <div id="slide1" class="carousel-item relative w-full"> 
+        <img src="https://i.ibb.co/CVsgKM4/national-cancer-institute-701-FJcj-LAQ-unsplash.jpg?w=800&h=300&hash=8B7BCDC2"  class="w-full" />
+        <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+          <a href="#slide4" class="btn btn-circle">❮</a> 
+          <div className='pe-100'><h1 className='text-primary text-5xl font-bold mb-5'>Visit your doctor</h1><h1 className='text-accent text-5xl font-bold mb-5'>Stay Happy One</h1><button class="btn btn-primary">Button</button></div>
+          <a href="#slide2" class="btn btn-circle">❯</a>
+        </div>
+      </div> 
+      <div id="slide2" class="carousel-item relative w-full">
+        <img src="https://i.ibb.co/s2sdcxJ/sander-sammy-38-Un6-Oi5be-E-unsplash.jpg?w=800&h=300&hash=500B67FB" class="w-full" />
+        <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+          <a href="#slide1" class="btn btn-circle">❮</a> 
+          <a href="#slide3" class="btn btn-circle">❯</a>
+        </div>
+      </div> 
+      <div id="slide3" class="carousel-item relative w-full">
+        <img src="https://i.ibb.co/n8HcXGj/markus-frieauff-IJ0-Ki-Xl4uys-unsplash.jpg?w=800&h=300&hash=A89D0DE6" class="w-full" />
+        <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+          <a href="#slide2" class="btn btn-circle">❮</a> 
+          <a href="#slide4" class="btn btn-circle">❯</a>
+        </div>
+      </div> 
     </div>
-  </div>
-</div>
     );
 };
 
---
+++ src/Pages/Home/Services.js
@@ -0,0 +1,64 @@
+import React from 'react';
+
+
+const Services = () => {
+    return (
+        <div className="container mx-auto md:px-20 px-12 py-6">
+        <h1 className="text-5xl text-primary text-center font-bold mt-10">
+          OUR SERVICES
+        </h1>
+        <div className="grid md:grid-cols-2 sm:grid-cols-1 xl:grid-cols-2 sm:gap-16 md:gap-8 my-12 text-start">
+          <div className="p-10 rounden dark:text-slate-200">
+            <h1 className="text-xl font-bold">World Wide Transport</h1>
+            <p className="text-sm  my-4">
+              Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+              nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+              erat, sed diam voluptua.
+            </p>
+          </div>
+          <div className="p-10 rounden dark:text-slate-200">
+            <h1 className="text-xl font-bold">World Wide Transport</h1>
+            <p className="text-sm  my-4">
+              Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+              nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+              erat, sed diam voluptua.
+            </p>
+          </div>
+          <div className="p-10 rounden dark:text-slate-200">
+            <h1 className="text-xl font-bold">World Wide Transport</h1>
+            <p className="text-sm  my-4">
+              Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+              nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+              erat, sed diam voluptua.
+            </p>
+          </div>
+          <div className="p-10 rounden dark:text-slate-200">
+            <h1 className="text-xl font-bold">World Wide Transport</h1>
+            <p className="text-sm  my-4">
+              Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+              nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+              erat, sed diam voluptua.
+            </p>
+          </div>
+          <div className="p-10 rounden dark:text-slate-200">
+            <h1 className="text-xl font-bold">World Wide Transport</h1>
+            <p className="text-sm  my-4">
+              Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+              nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+              erat, sed diam voluptua.
+            </p>
+          </div>
+          <div className="p-10 rounden dark:text-slate-200">
+            <h1 className="text-xl font-bold">World Wide Transport</h1>
+            <p className="text-sm  my-4">
+              Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+              nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+              erat, sed diam voluptua.
+            </p>
+          </div>
+        </div>
+        </div>
+  );
+};
+
+export default Services;
