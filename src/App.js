0d28b7d69a5d1881de6d2cdd4308cb09ef20447c
devnoman01 <devnoman01@gmail.com>
2022-06-29 20:26:24 +0600

    doctor profile page complete

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
-import './App.css';
-import { Routes, Route} from "react-router-dom";
-import Navbar from './Pages/Shared/Navbar';
-import Home from './Pages/Home/Home';
-import About from './Pages/About/About';
-import Login from './Pages/Login/Login';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./Pages/Shared/Navbar";
+import Home from "./Pages/Home/Home";
+import About from "./Pages/About/About";
+import Login from "./Pages/Login/Login";
+import DoctorProfile from "./Pages/DoctorProfile/DoctorProfile";
 
 function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
-      
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="about" element={<About />} />
         <Route path="login" element={<Login />} />
+        <Route path="doctor111" element={<DoctorProfile />} />
       </Routes>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -15,9 +15,8 @@ module.exports = {
           "base-100": "#ffffff",
         },
       },
-      "dark",
-      "cupcake",
+      "light",
     ],
   },
   plugins: [require("daisyui")],
-}
+};
