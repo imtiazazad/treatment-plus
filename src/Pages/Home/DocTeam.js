1ab5c09704fdf09bdcc1d5aaab307dd270597829
imtiazazad <imtiaz16-549@diu.edu.com>
2022-07-08 14:46:46 +0600

    docsteam

--- src/Pages/Home/DocTeam.js
+++ src/Pages/Home/DocTeam.js
@@ -1,51 +1,154 @@
 import React from 'react';
+import img1 from "../../images/img1.jpg";
+import img2 from "../../images/img2.jpg";
+import img3 from "../../images/img3.jpg";
 
 
-
-
-const DocTeam = ({ doctor }) => {
-  const {name, position} = doctor;
- 
+const DocTeam = () => {
+  
     return (
-      <>
-       <div className="card lg:max-w-lg bg-base-100 shadow-xl">
-            
-            <div className="card-body items-center text-center">
-                <h2 className="card-title">{name}</h2>
-                <p>{position}</p>
-                <div className="card-actions">
-                <button  className="btn btn-primary">Buy Now</button>
-                </div>
-            </div>
-        </div>
-
-
-      {/* <div class="carousel w-full">
-  <div id="item" class="carousel-item">
+        <div className='mb-20'>
+        <div class="carousel w-full">
+  <div id="item1" class="carousel-item">
   <div class="card w-96 bg-base-100 shadow-xl ml-14">
-  <figure><img src={img1} alt="Shoes" /></figure> */}
-  {/* <div class="card-body">
-    <h2 class="card-title">{name}</h2>
-    <h3 className='text-start font-bold mb-5'>{position}</h3>
+  <figure><img src={img1} alt="Shoes" /></figure>
+  <div class="card-body">
+    <h2 class="card-title">DR. bruselin</h2>
+    <h3 className='text-start font-bold mb-5'>Dentish</h3>
     <p className='text-start'>Monday-Friday<span className='pl-32'>8:30-17:00</span></p>
     <p className='text-start'>Saturday   <span className='pl-40'>10:00-16:00</span></p>
     <p className='text-start'>Sunday<span className='pl-44'>12:00-14:00</span></p>
     <div class="card-actions justify-end">
-      <button 
-      
-      class="btn btn-primary mt-5">Read More</button>
+      <button class="btn btn-primary mt-5">Read More</button>
     </div>
-  </div> */}
-{/* </div>
+  </div>
 </div>
-</div> */}
+<div class="card w-96 bg-base-100 shadow-xl ml-10">
+  <figure><img src={img2} alt="Shoes" /></figure>
+  <div class="card-body">
+  <h2 class="card-title">DR. bruselin</h2>
+    <h3 className='text-start font-bold mb-5'>Dentist</h3>
+    <p className='text-start'>Monday-Friday<span className='pl-32'>8:30-17:00</span></p>
+    <p className='text-start'>Saturday   <span className='pl-40'>10:00-16:00</span></p>
+    <p className='text-start'>Sunday<span className='pl-44'>12:00-14:00</span></p>
+    <div class="card-actions justify-end">
+    <button class="btn btn-primary mt-5">Read More</button>
+    </div>
+  </div>
+</div>
 
-{/* 
+<div class="card w-96 bg-base-100 shadow-xl ml-10">
+  <figure><img src={img3} alt="Shoes" /></figure>
+  <div class="card-body">
+  <h2 class="card-title">DR. bruselin</h2>
+    <h3 className='text-start font-bold mb-5'>Dentist</h3>
+    <p className='text-start'>Monday-Friday<span className='pl-32'>8:30-17:00</span></p>
+    <p className='text-start'>Saturday   <span className='pl-40'>10:00-16:00</span></p>
+    <p className='text-start'>Sunday<span className='pl-44'>12:00-14:00</span></p>
+    <div class="card-actions justify-end">
+    <button class="btn btn-primary mt-5">Read More</button>
+    </div>
+  </div>
+</div>
+  </div> 
+  <div id="item2" class="carousel-item">
+  <div class="card w-96 bg-base-100 shadow-xl ml-14">
+  <figure><img src={img1} alt="Shoes" /></figure>
+  <div class="card-body">
+    <h2 class="card-title">DR. bruselin</h2>
+    <h3 className='text-start font-bold mb-5'>Dentist</h3>
+    <p className='text-start'>Monday-Friday<span className='pl-32'>8:30-17:00</span></p>
+    <p className='text-start'>Saturday   <span className='pl-40'>10:00-16:00</span></p>
+    <p className='text-start'>Sunday<span className='pl-44'>12:00-14:00</span></p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary mt-5">Read More</button>
+
+    </div>
+  </div>
+</div>
+
+<div class="card w-96 bg-base-100 shadow-xl ml-10">
+  <figure><img src={img2} alt="Shoes" /></figure>
+  <div class="card-body">
+  <h2 class="card-title">Dr Makota Brown</h2>
+    <h3 className='text-start font-bold mb-5'>Therapist</h3>
+    <p className='text-start'>Monday-Friday<span className='pl-32'>8:30-17:00</span></p>
+    <p className='text-start'>Saturday   <span className='pl-40'>10:00-16:00</span></p>
+    <p className='text-start'>Sunday<span className='pl-44'>12:00-14:00</span></p>
+    <div class="card-actions justify-end">
+    <button class="btn btn-primary mt-5">Read More</button>
+    </div>
+  </div>
+</div>
+
+<div class="card w-96 bg-base-100 shadow-xl ml-10">
+  <figure><img src={img3} alt="Shoes" /></figure>
+  <div class="card-body">
+  <h2 class="card-title">Dr Makota Brown</h2>
+    <h3 className='text-start font-bold mb-5'>Therapist</h3>
+    <p className='text-start'>Monday-Friday<span className='pl-32'>8:30-17:00</span></p>
+    <p className='text-start'>Saturday   <span className='pl-40'>10:00-16:00</span></p>
+    <p className='text-start'>Sunday<span className='pl-44'>12:00-14:00</span></p>
+    <div class="card-actions justify-end">
+    <button class="btn btn-primary mt-5">Read More</button>
+    </div>
+  </div>
+</div>
+  </div> 
+  <div id="item3" class="carousel-item">
+  <div class="card w-96 bg-base-100 shadow-xl ml-14">
+  <figure><img src={img1} alt="Shoes" /></figure>
+  <div class="card-body">
+    <h2 class="card-title">Dr Makota Brown</h2>
+    <h3 className='text-start font-bold mb-5'>Therapist</h3>
+    <p className='text-start'>Monday-Friday<span className='pl-32'>8:30-17:00</span></p>
+    <p className='text-start'>Saturday   <span className='pl-40'>10:00-16:00</span></p>
+    <p className='text-start'>Sunday<span className='pl-44'>12:00-14:00</span></p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary mt-5">Read More</button>
+
+    </div>
+  </div>
+</div>
+
+<div class="card w-96 bg-base-100 shadow-xl ml-10">
+  <figure><img src={img2} alt="Shoes" /></figure>
+  <div class="card-body">
+  <h2 class="card-title">Dr Makota Brown</h2>
+    <h3 className='text-start font-bold mb-5'>Therapist</h3>
+    <p className='text-start'>Monday-Friday<span className='pl-32'>8:30-17:00</span></p>
+    <p className='text-start'>Saturday   <span className='pl-40'>10:00-16:00</span></p>
+    <p className='text-start'>Sunday<span className='pl-44'>12:00-14:00</span></p>
+    <div class="card-actions justify-end">
+    <button class="btn btn-primary mt-5">Read More</button>
+    </div>
+  </div>
+</div>
+
+<div class="card w-96 bg-base-100 shadow-xl ml-10">
+  <figure><img src={img3} alt="Shoes" /></figure>
+  <div class="card-body">
+  <h2 class="card-title">Dr Makota Brown</h2>
+    <h3 className='text-start font-bold mb-5'>Therapist</h3>
+    <p className='text-start'>Monday-Friday<span className='pl-32'>8:30-17:00</span></p>
+    <p className='text-start'>Saturday   <span className='pl-40'>10:00-16:00</span></p>
+    <p className='text-start'>Sunday<span className='pl-44'>12:00-14:00</span></p>
+    <div class="card-actions justify-end">
+    <button class="btn btn-primary mt-5">Read More</button>
+    </div>
+  </div>
+</div>
+
+
+  </div> 
+</div> 
 <div class="flex justify-center w-full py-2 gap-2">
-  <a href="#item" class="btn btn-xs">1</a> 
-</div> */}
+  <a href="#item1" class="btn btn-xs">1</a> 
+  <a href="#item2" class="btn btn-xs">2</a> 
+  <a href="#item3" class="btn btn-xs">3</a> 
+</div>
+</div>
 
-</>
     );
 }
 
--- src/Pages/Home/Doctors.js
+++ src/Pages/Home/Doctors.js
@@ -1,33 +1,25 @@
-import React, { useEffect, useState } from 'react';
-import DocTeam from './DocTeam';
+// import React from 'react';
+// import DocTeam from './DocTeam';
 
-const Doctors = () => {
-    const [doctors, setDoctors] = useState([]);
+// const Doctors = () => {
+//     const [doctors, setDoctors] = useState([]);
 
-    useEffect(() => {
-        fetch('doctors.json')
-            .then(res => res.json())
-            .then(data => setDoctors(data));
-    }, [])
+//   useEffect(() => {
+//       fetch('doctors.json')
+//           .then(res => res.json())
+//           .then(data => setDoctors(data));
+//         }, [])
+        
+//     return (
+//         <div>
+//              {
+//                     doctors.map(doctor => <DocTeam
+//                     key={doctor._id}
+//                     doctor={doctor}
+//                     ></DocTeam>)
+//                 }
+//         </div>
+//     );
+// };
 
-    return (
-        <div className='my-28'>
-            <div className='text-center'>
-            <h3 className='text-primary text-xl font-bold uppercase'>Our Electric Tools</h3>
-            <h2 className='text-4xl'>Tools That will Be Useful</h2>
-            </div>
-            <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
-            
-                        {
-                            doctors.map(doctor => <DocTeam
-                                key={doctor._id}
-                                doctor={doctor}
-                            ></DocTeam>)
-                        }
-                    </div>
-                    </div>
-            
-    );
-};
-
-export default Doctors;
+// export default Doctors;
